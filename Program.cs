7d606cdffa562f6b9f540139f7b6275be4d09236
student <student@C251U10>
2020-09-11 14:23:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace praktika_02_upr2_11._09._2020
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s = "Microsoft .NET Framework 2 Application Development Foundation";
+      string[] sa = s.Split("");
+      Array.Sort(sa);
+      s = string.Join(" ", sa); 
+      Console.WriteLine(s);
+    }
+  }
+}
